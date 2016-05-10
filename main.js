8d46e67ca42d1721514e45dad26796fd62f9ee20
Rongrong Luo <luo4188@gmail.com>
2016-05-11 00:30:30 +1200

    calculagraph service

--- main.js
+++ main.js
@@ -3,8 +3,17 @@
 	angular
 		.module('mainModule', ['ui.bootstrap', 'ngRoute'])
 		.controller('mainController', mainController);
-	mainController.$inject = ["$scope", "$q", "$log"];
-	function mainController($scope, $q, $log) {
-
+	mainController.$inject = ["$scope", "$q", "$log", "calculagraphService"];
+	function mainController($scope, $q, $log, calculagraphService) {
+		var testFn = function() {
+			var array = [];
+			array[300000000] = 1;
+			for(var i = 0; i < array.length; i++) {
+				
+			}
+		}
+		
+		$scope.result = calculagraphService.calculateElapsedTime(testFn);
+		$log.debug($scope.result);
 	}
 })();
